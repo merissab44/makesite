1a07c6225a43c01e672343424d6ad3651209aa65
Merissa Bridgeman <merissabridgeman@Merissas-MacBook-Pro-2.local>
2022-02-14 21:54:33 -0800

    rendered a new html file

--- makesite.go
+++ makesite.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+        "html/template"
+        "os"
+		"io/ioutil"
+)
+
+// Page holds all the information we need to generate a new
+// HTML page from a text file on the filesystem.
+type Page struct {
+    TextFilePath string
+    TextFileName string
+    HTMLPagePath string
+    Content      string
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	filePath := "filePath"
+	// read the file
+	fileContents, err := ioutil.ReadFile("first-post.txt")
+	// creating a new Page object 
+    page := Page{
+      TextFilePath: filePath,
+      TextFileName: "first",
+      HTMLPagePath: "first-post.html",
+      Content:      string(fileContents),
+    }
+   // create the template t
+    t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+    // Create a new, blank HTML file.
+    newFile, err := os.Create("first-post.html")
+    if err != nil {
+          panic(err)
+    }
+    // inject the newly created page into the new htmlfile named new.html
+    t.Execute(newFile, page)
 }
