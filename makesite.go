6b2be36b0513eecdcbc6e461ea661a6aa5c002dc
Merissa Bridgeman <merissabridgeman@Merissas-MacBook-Pro-2.local>
2022-02-22 20:12:01 -0800

    finished v1.1

--- makesite.go
+++ makesite.go
@@ -20,40 +20,45 @@ type Page struct {
 
 func main() {
 	filePath := "filePath"
-	// read the file
-	fileContents, err := ioutil.ReadFile("first-post.txt")
 	//create flag for file
 	filePtr := flag.String("file", "", "file to read")
+	//create flag for directory
+	dirPtr := flag.String("dir", "", "directory to read from")
 	flag.Parse()
 
-	dirPtr := flag.String("dir", "", "directory to read")
-	flag.Parse()
-
-	files, err := ioutil.ReadDir(*dirPtr)
-
-	// print all the files
-	for _, file := range files {
-		fmt.Println(file.Name())
+	if *dirPtr != "" {
+		files, err := ioutil.ReadDir(*dirPtr)
+		if err != nil {
+			fmt.Println(err)
+		}
+		for _, file := range files {
+			// read the file
+			fileContents, err := ioutil.ReadFile(file.Name())
+			if err != nil {
+				fmt.Println("Error reading file:", err)
+				continue
+			}
+			// if the file is a text file
+			if strings.HasSuffix(file.Name(), ".txt") {
+				// replace text file with html file
+				filepath := strings.Replace(file.Name(), ".txt", ".html", 1)
+				page := Page{
+					TextFilePath: filePath,
+					TextFileName: *filePtr,
+					HTMLPagePath: filepath,
+					Content:      string(fileContents),
+				}
+				// create the template t
+				t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+				// Create a new, blank HTML file.
+				newFile, err := os.Create(filepath)
+				if err != nil {
+					panic(err)
+				}
+				// inject the newly created page into the new htmlfile template
+				t.Execute(newFile, page)
+			}
+			continue
+		}
 	}
-
-	// replace the .txt with .html
-	htmlpath := func() string {
-		return strings.Replace(*filePtr, ".txt", ".html", -1)
-	}
-	// creating a new Page object
-	page := Page{
-		TextFilePath: filePath,
-		TextFileName: "first",
-		HTMLPagePath: htmlpath(),
-		Content:      string(fileContents),
-	}
-	// create the template t
-	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
-	// Create a new, blank HTML file.
-	newFile, err := os.Create(htmlpath())
-	if err != nil {
-		panic(err)
-	}
-	// inject the newly created page into the new htmlfile named new.html
-	t.Execute(newFile, page)
 }
